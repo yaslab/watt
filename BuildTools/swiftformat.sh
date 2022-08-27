6a22310354a5f1b09582b97aeffd5ecbb48b8aa8
Yasuhiro Hatta <hatta.yasuhiro@gmail.com>
2022-08-27 22:39:40 +0900

    Add SwiftFormat

---
+++ BuildTools/swiftformat.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+set -eu
+
+TOOL_DIR="$(cd "$(dirname "$0")"; pwd)"
+ROOT_DIR="$(dirname "$TOOL_DIR")"
+
+function run_swiftformat() {
+    local ARG_SRC_DIR="$1"
+    swift run -c release --package-path "$TOOL_DIR" \
+        swiftformat "$ROOT_DIR/$ARG_SRC_DIR"
+}
+
+run_swiftformat Watt
--- Watt/WattAppDelegate.swift
+++ Watt/WattAppDelegate.swift
@@ -9,15 +9,15 @@ import Cocoa
 
 class WattAppDelegate: NSObject, NSApplicationDelegate {
     private lazy var controller = WattAppController()
-    
+
     // MARK: - Launching Applications
-    
+
     func applicationDidFinishLaunching(_ notification: Notification) {
         controller.closeWindow()
     }
-    
+
     // MARK: - Terminating Applications
-    
+
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return false
     }
