d1f2a201a3eb513a4ef5e2b7f9dce5d23fab8e6a
Yasuhiro Hatta <hatta.yasuhiro@gmail.com>
2022-08-27 21:49:26 +0900

    Create new macOS project with Xcode 13.4.1

---
+++ Watt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Watt
+//
+//  Created by Yasuhiro Hatta on 2022/08/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Watt/WattApp.swift
@@ -0,0 +1,17 @@
+//
+//  WattApp.swift
+//  Watt
+//
+//  Created by Yasuhiro Hatta on 2022/08/27.
+//
+
+import SwiftUI
+
+@main
+struct WattApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
