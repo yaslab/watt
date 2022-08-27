c34683074b5e6785734337d84a120dc8d2506b74
Yasuhiro Hatta <hatta.yasuhiro@gmail.com>
2022-08-28 01:47:52 +0900

    Create launcher project with Xcode 13.4.1

---
+++ WattLauncher/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WattLauncher
+//
+//  Created by Yasuhiro Hatta on 2022/08/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WattLauncher/WattLauncherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WattLauncherApp.swift
+//  WattLauncher
+//
+//  Created by Yasuhiro Hatta on 2022/08/28.
+//
+
+import SwiftUI
+
+@main
+struct WattLauncherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
