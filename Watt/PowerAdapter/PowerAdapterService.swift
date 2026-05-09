b0a02e8f7750d87ecc38131ee23ae4d520864cbb
Yasuhiro Hatta <hatta.yasuhiro@gmail.com>
2026-05-09 19:45:41 +0900

    [Refactoring] Separate the protocol and implementation files

--- Watt/PowerAdapter/PowerAdapterService.swift
+++ Watt/PowerAdapter/PowerAdapterService.swift
@@ -6,48 +6,8 @@
 //
 
 import Combine
-import Foundation
 
 protocol PowerAdapterService {
     func adapter() -> PowerAdapter
     func notifications() -> AnyPublisher<Void, Never>
-}
-
-class PowerAdapterServiceLiveImpl: PowerAdapterService {
-    private func convert(
-        details: ExternalPowerAdapterDetails?,
-        descriptions: [PowerSourceDescription]?
-    ) -> PowerAdapter {
-        return PowerAdapter(
-            wattage: details?.watts.map { Wattage(rawValue: $0) },
-            voltage: details?.voltage.map { Voltage(rawValue: $0) },
-            current: details?.current.map { Current(rawValue: $0) },
-            name: details?.name?.trimmingCharacters(in: .whitespaces),
-            manufacturer: details?.manufacturer?.trimmingCharacters(in: .whitespaces),
-            sources: descriptions?.map { description in
-                PowerAdapter.PowerSource(
-                    id: description.powerSourceID,
-                    state: description.powerSourceState,
-                    voltage: description.voltage.map { Voltage(rawValue: $0) },
-                    current: description.current.map { Current(rawValue: $0) },
-                    batteryHealth: description.batteryHealth,
-                    batteryCurrentCapacity: description.currentCapacity,
-                    batteryTimeToEmpty: description.timeToEmpty.map { TimeInMinutes(rawValue: $0) },
-                    batteryTimeToFullCharge: description.timeToFullCharge.map { TimeInMinutes(rawValue: $0) },
-                    isBatteryCharging: description.isCharging
-                )
-            } ?? []
-        )
-    }
-
-    func adapter() -> PowerAdapter {
-        return convert(
-            details: PowerSource.externalPowerAdapterDetails(),
-            descriptions: PowerSource.powerSources()
-        )
-    }
-
-    func notifications() -> AnyPublisher<Void, Never> {
-        return PowerSourceNotification.publisher(for: .any)
-    }
 }
